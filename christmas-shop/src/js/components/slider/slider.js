0c179a5b3fb99089927640d339a23957e815e56e
Anastasia Savrukhina <assavr0@gmail.com>
2024-11-28 12:24:13 +0300

    refactor: delete svg code from slider.js

--- christmas-shop/src/js/components/slider/slider.js
+++ christmas-shop/src/js/components/slider/slider.js
@@ -139,38 +139,3 @@ export class Slider extends BaseElement {
     this.append(gratitude, gratitudeBottom, sliderContainer);
   }
 }
-
-{
-  /* <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
->
-  <path
-    d="M6 12H18.5M18.5 12L12.5 6M18.5 12L12.5 18"
-    stroke="white"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-</svg>; */
-}
-
-{
-  /* <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
->
-  <path
-    d="M18.5 12H6M6 12L12 6M6 12L12 18"
-    stroke="white"
-    stroke-opacity="0.4"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-</svg>; */
-}
