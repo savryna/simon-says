39ba703938cbec92e0332f558fe82f8079f08726
Anastasia Savrukhina <assavr0@gmail.com>
2025-01-09 22:20:42 +0300

    fix: remove christmas-shop folder

--- christmas-shop/src/js/gift.js
+++
@@ -1,4 +0,0 @@
-import { RootGifts } from './components/wrapper/wrapper.js';
-
-const root = new RootGifts();
-root.init();
--- christmas-shop/src/js/home.js
+++
@@ -1,4 +0,0 @@
-import { Root } from './components/wrapper/wrapper.js';
-
-const root = new Root();
-root.init();
